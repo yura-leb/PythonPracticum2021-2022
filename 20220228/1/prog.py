d5df621868f8681e933bb69c63f47fc7068574eb
yura-leb <lebedinsliyyue@gmail.com>
2022-03-04 10:59:50 +0300

    Third string parameter

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,12 +1,11 @@
 import textdistance
 
-def dist(s1, s2):
+def dist(s1, s2, s3):
     lev = textdistance.Levenshtein()
     return lev(s1, s2)
 
 s1 = input()
 s2 = input()
-res = dist(s1, s2)
-
 s3 = input()
+res = dist(s1, s2, s3)
 
