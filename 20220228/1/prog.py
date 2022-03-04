7beb7158fd7f8e76f15688343485d693e0c55345
yura-leb <lebedinsliyyue@gmail.com>
2022-03-04 11:13:18 +0300

    D or L parameter

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,11 +1,16 @@
 import textdistance
 
 def dist(s1, s2, s3):
-    lev = textdistance.Levenshtein()
-    return lev(s1, s2)
+    if s3 == "L":
+        distance = textdistance.Levenshtein()
+    elif s3 == "D":
+        distanse = textdistance.DamerauLevenshtein()
+    else:
+        return -1
+    return distance(s1, s2)
 
-s1 = input()
-s2 = input()
-s3 = input()
+s1 = input().strip('\n')
+s2 = input().strip('\n')
+s3 = input().strip('\n')
 res = dist(s1, s2, s3)
 
