38fbf96ccd24c84a6f022295f97394eda7eaa397
Лебединский Юрий practicum <s02190141@stud.cs.msu.ru>
2022-05-16 13:23:42 +0300

    Classwork

---
+++ 20220516/1/dodo.py
@@ -0,0 +1,43 @@
+DOIT_CONFIG = {'default_tasks': ['babel', 'test', 'wheel', 'sdist', 'cleanup']}
+
+def task_babel():
+    """Make translation po file."""
+    return {
+            "actions": [
+                "pybabel extract -o po/prog.pot prog.py",
+                "pybabel update -D prog -i po/prog.pot -d po -l ru",
+                "pybabel compile -D prog -d po -l ru"
+            ],
+            "file_dep": ["prog.py"],
+            "targets": ["po/prog.pot", "po/ru/LC_MESSAGES/prog.mo"]
+           }
+
+def task_wheel():
+    """Create wheel."""
+    return {
+            "actions": ['python3 -m build -w'],
+            "file_dep": ["prog.py", "po/ru/LC_MESSAGES/prog.mo"],
+    }
+
+def task_sdist():
+    """Create tar."""
+    return {
+            "actions": ['python3 -m build -s'],
+            "file_dep": ["prog.py", "po/ru/LC_MESSAGES/prog.mo"],
+    }
+
+
+def task_test():
+    """Test prog.py."""
+    return {
+        "actions": ["python3 -m unittest -v"],
+        #"file_dep": ["test_prog.py"]
+    }
+
+
+def task_cleanup():
+    """Remove all."""
+    return {
+            "actions": ["rm -f po/prog.pot", "rm -f po/ru/LC_MESSAGES/prog.mo"],
+   }
+
---
+++ 20220516/1/test_prog.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from prog import solve
+
+
+class TestSolver(unittest.TestCase):
+
+    def test_not_zero_1(self):
+        self.assertEqual(solve(1, -1), 1.0)
+
+    def test_zero(self):
+        self.assertEqual(solve(0, 2), None)
+
+    def test_not_zero_2(self):
+        self.assertEqual(solve(1, 1), -1.0)
